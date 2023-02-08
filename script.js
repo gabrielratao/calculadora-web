f8456f89914b578f349ee45be1a8c6f5a1f117ca
gabrielratao <gabiratao28@gmail.com>
2023-02-08 12:05:23 -0300

    first step js file

---
+++ script.js
@@ -0,0 +1,83 @@
+//importando os elementos
+
+
+const showResult = document.getElementById('total-result')
+
+//digitação
+const toClear = document.getElementById('clear-numbers')
+const deleteNumber = document.getElementById('delete-number')
+const addMinusPlus = document.getElementById('add-minus-or-plus')
+const addComma = document.getElementById('add-comma')
+
+//números
+const numberZero = document.getElementById('number-zero')
+const numberOne = document.getElementById('number-one')
+const numberTwo = document.getElementById('number-two')
+const numberThree = documet.getElementById('number-three')
+const numberFour = document.getElementById('number-four')
+const numberFive = document.getElementById('number-five')
+const numberSix = document.getElementById('number-six')
+const numberSeven = document.getElementById('number-seven')
+const numberEight = document.getElementById('number-eight')
+const numberNine = document.getElementById('number-nine')
+
+//operações matemáticas
+const calculatePercentage = document.getElementById('calculate-percentage')
+const calculateDivision = document.getElementById('calculate-division')
+const calculateMultiplication = document.getElementById('calculate-multiplication')
+const calculateSubtraction = document.getElementById('calculate-subtraction')
+const calculateAddition = document.getElementById('calculate-addition')
+const equal = document.getElementById('calculate')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
